950fa5417cf86dd9653faf695d2a0d95c91e8110
徐青山 <1370572852@qq.com>
2025-07-10 18:34:12 +0800

    1

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import { Button } from '../components/button';
 
 export default function Home({ allPostsData }) {
   return (
@@ -18,6 +19,7 @@ export default function Home({ allPostsData }) {
           <a href="https://nextjs.org/learn/pages-router">our Next.js tutorial</a>.)
         </p>
       </section>
+      <Button />
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
